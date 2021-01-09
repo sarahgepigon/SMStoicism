3be4a2059d1b0554364ffb503e5db4e17b196781
Sarah Gepigon <70664383+sarahgepigon@users.noreply.github.com>
2021-01-09 18:08:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,43 @@
+#import shortest to longest module
+import os
+import random
+import requests
+from twilio.rest import Client
+
+#pulled Stoicism quotes from the internet using a 
+#philosophy quotes API in python https://github.com/KaranDahiya/philosophy-quotes-API
+
 # https://realpython.com/api-integration-in-python/
 
-import requests
-
+#get all Stoicism quotes from this list of dictionaries
 resp = requests.get('https://philosophy-quotes-api.glitch.me/quotes/philosophy/Stoicism')
-# why do i need the / at the end? 
 
 if resp.status_code != 200:
 	#This means something went wrong. 
 	raise ApiError('GET /Stoicism {}'.format(resp.status_code))
 
+#pick single quote from API response
 
-for quote in resp.json():
+random_dictionary = random.choice(resp.json())
 
-	print('{} - {}'.format(quote['quote'], quote['source']))
+text_message = '"{}" - {}'.format(random_dictionary['quote'], random_dictionary['source'])
+
+
+# send a quote to my phone via Twilio
+
+#"Send an outbound SMS with Python" https://www.twilio.com/docs/sms/quickstart/python#install-python-and-the-twilio-helper-library
+
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+#create variables for secure #s
+twilio_phone_number = os.environ['TWILIO_PHONE_NUMBER']
+my_phone_number = os.environ['MY_PHONE_NUMBER']
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+				.create(
+					body=text_message,
+					from_=twilio_phone_number,
+					to=my_phone_number
+					)
+
